cb6e700c5a67de915ce02bae8d4bdc4d2ee1ed7c
HALİL İBRAHİM DÜRMÜŞ <111151272+halilibrahimd27@users.noreply.github.com>
2025-04-08 23:06:13 +0300

    Add files via upload

---
+++ security-event-manager/src/main/java/com/security/eventmanager/security_event_manager/SecurityEventManagerApplication.java
@@ -0,0 +1,13 @@
+package com.security.eventmanager.security_event_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityEventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityEventManagerApplication.class, args);
+	}
+
+}
---
+++ security-event-manager/src/test/java/com/security/eventmanager/security_event_manager/SecurityEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.eventmanager.security_event_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityEventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
